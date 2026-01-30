c1a2df159746603184ad8a0dd76ba8342710426f
Keppiekip <maharjanitika@gmail.com>
2026-01-30 14:21:50 +0545

    add title

--- src/components/notes/NoteItem.js
+++ src/components/notes/NoteItem.js
@@ -11,7 +11,7 @@ export default function NoteItem(props) {
     <>
       <div className="card" data-bs-theme={`${theme==="light"?"light":"dark"}`} style={{width: "100%", height: "192px"}} onClick={props.OpenNoteDetailModal}>
           <div className="card-body">
-              <h5 className="card-title" style={{color: `${theme==="light"?"black":"white"}`}}>{trimTitle(props.note.title)}</h5>
+              <h5 className="card-title" style={{color: `${theme==="light"?"black":"white"}`}} title={props.note.title}>{trimTitle(props.note.title)}</h5>
               <div className="mb-2" style={{display: "flex"}}>
                 <p className="card-subtitle text-body-secondary" style={{margin: "0px", padding: "0px", fontSize: "14px", fontWeight: "500"}}>{handleCapitalizeFirstLetter(props.note.tag)}</p>
                 <p className="card-subtitle text-body-secondary mx-1" style={{margin: "0px", padding: "0px", fontSize: "14px"}}>|</p>
